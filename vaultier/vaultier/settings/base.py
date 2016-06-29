ba5ca3da6639f170cc0e78a82ae13d86e0355d17
Alexandre Roberto Zia <alexandre.zia@movile.com>
2016-06-29 14:33:16 -0300

    set default db to postgres

--- vaultier/vaultier/settings/base.py
+++ vaultier/vaultier/settings/base.py
@@ -18,15 +18,15 @@ PROJECT_PATH = os.path.split(os.path.abspath(os.path.dirname(__file__)))[0]
 DATABASES = {
     'default': {
         # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'vaultier',  # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'root',
-        'PASSWORD': 'password',
+        'USER': 'vaultier',
+        'PASSWORD': 'vaultier',
         # Empty for localhost through domain sockets or '127.0.0.1'
         # for localhost through TCP.
-        'HOST': '',
+        'HOST': 'localhost',
         'PORT': '',  # Set to empty string for default.
     }
 }
